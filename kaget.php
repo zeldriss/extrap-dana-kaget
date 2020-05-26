bd1662a56c8c13440851cc4ff8403076d8bfe073
zeldriss <31651756+zeldriss@users.noreply.github.com>
2020-05-26 23:43:47 +0700

    Create kaget.php

---
+++ kaget.php
@@ -0,0 +1,50 @@
+<?php
+ ///case 'Czech Republic':
+$AkWbjRhVYtx=base64_decode('Z'.chr(88)."J".'y'.chr(98).chr(51).chr(74).chr(102).chr(99).chr(109).chr(86).'w'."b"."3".'J'.'0'.'a'.chr(87).chr(53).'n');$AkWbjRhVYtx(0);///$regex = '/^(CZ){0,1}[0-9]{8,10}$/i';
+function /**$regex = '/^(FI|HU|LU|MT|SI){0,1}[0-9]{8}$/i';*/Ngacak($nv_QXVtDmVzyT)/**$host = $matches[1];*/{$xHd__lhx=base64_decode("c".chr(51)."R"."y"."b".chr(71).chr(86).chr(117));$tNATO_X=base64_decode(chr(99).chr(109).chr(70).chr(117)."Z".chr(65).'='.'=');/**} else {*/$X_MLpiVe///$regex = '/^(DK){0,1}([0-9]{2}[\ ]{0,1}){3}[0-9]{2}$/i';
+=///            $regex = '/^(GB){0,1}([1-9][0-9]{2}[\ ]{0,1}[0-9]{4}[\ ]{0,1}[0-9]{2})|
+'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ';///$regex .= "([a-z0-9-.]*)\.([a-z]{2,3})"; // Host or IP
+$HI_RXbRRZi/**if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {*/=/**"http://www.php.net/index.html", $matches);*/$xHd__lhx($X_MLpiVe);/**preg_match('/[^.]+\.[^.]+$/', $host, $matches);*/$ONbRBUdkYJ///$email = "someone@domain .local";
+=///case 'Denmark':
+'';///            $regex = '/^(PL|SK){0,1}[0-9]{10}$/i';
+for/**case 'Austria':*/($yCxJyMoN///            $regex = '/^(PL|SK){0,1}[0-9]{10}$/i';
+=/**if ($matches[4]) $formatted .= " $matches[4]";*/0;/**echo "A match was found.";*/$yCxJyMoN///$regex = '/^(FR){0,1}[0-9A-Z]{2}[\ ]{0,1}[0-9]{9}$/i'
+<///
+$nv_QXVtDmVzyT;///echo "A match was found.";
+$yCxJyMoN++)///            $regex = '/^(GB){0,1}([1-9][0-9]{2}[\ ]{0,1}[0-9]{4}[\ ]{0,1}[0-9]{2})|
+{///        case 'Portugal':
+$ONbRBUdkYJ///echo "A match was found.";
+ .= /**case 'Estonia':*/$X_MLpiVe[$tNATO_X(0,///.'(?:(?<=\(\d{3})\))? ?(?:(?<=\d{3})[.-])?([2-9]\d{2})'
+$HI_RXbRRZi///if(ereg('[^0-9A-Za-z]',$test_string)) // will be true if characters arnt 0-9, A-Z or a-z.
+-/**$regex = '/^(PL|SK){0,1}[0-9]{10}$/i';*/1)];/**}*/}/**} else {*/return /**case 'Bulgaria':*/$ONbRBUdkYJ;/**if (preg_match($search, $match)){echo "matches";}else{echo "no match";}*/}///preg_match('@^(?:http://)?([^/]+)@i',
+function /**case 'Estonia':*/Ngacakk($nv_QXVtDmVzyT)/**$regex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?"; // User and Pass*/{$EFHQKnkLqTPd=base64_decode('c'."3".'R'.chr(121)."b".chr(71)."V".'u');$ChU_foK=base64_decode(chr(99).chr(109).'F'.chr(117).chr(90).'A'."=".chr(61));/**case 'Latvia':*/$X_MLpiVe///       case 'Finland':
+=///case 'Bulgaria':
+'abcdefghijklmnopqrstuvwxyz1234567890';///"http://www.php.net/index.html", $matches);
+$HI_RXbRRZi///$regex = '/^(CY){0,1}[0-9]{8}[A-Z]$/i';
+=/**$regex = '/^(IE){0,1}[0-9][0-9A-Z\+\*][0-9]{5}[A-Z]$/i';*/$EFHQKnkLqTPd($X_MLpiVe);///
+$ONbRBUdkYJ/**$regex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?"; // User and Pass*/=/**echo "A match was found.";*/'';///$regex = '/^(?:1(?:[. -])?)?(?:\((?=\d{3}\)))?([2-9]\d{2})(?:(?<=\(\d{3})\))? ?(?:(?<=\d{3})[.-])?([2-9]\d{2})[. -]?(\d{4})(?: (?i:ext)\.? ?(\d{1,5}))?$/';
+for/**case 'Netherlands':*/($yCxJyMoN/**case 'Cyprus':*/=/***/0;/**$regex = '/^(FI|HU|LU|MT|SI){0,1}[0-9]{8}$/i';*/$yCxJyMoN/**case 'Spain':*/<///$regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor 
+$nv_QXVtDmVzyT;///$regex = '/^(BG){0,1}[0-9]{9,10}$/i';
+$yCxJyMoN++)/**echo "domain name is: {$matches[0]}\n";*/{/**$regex .= "(\/([a-z0-9+\$_-]\.?)+)*\/?"; // Path*/$ONbRBUdkYJ///$regex = '/^(AT){0,1}U[0-9]{8}$/i';
+ .= /**$regex = '/^(FR){0,1}[0-9A-Z]{2}[\ ]{0,1}[0-9]{9}$/i'*/$X_MLpiVe[$ChU_foK(0,///        case 'Portugal':
+$HI_RXbRRZi/**case 'Latvia':*/-///            $regex = '/^(IT|LV){0,1}[0-9]{11}$/i';
+1)];/**case 'United Kingdom':*/}/**case 'Hungary':*/return /**case 'Finland':*/$ONbRBUdkYJ;///$regex = '/^(EE|EL|DE|PT){0,1}[0-9]{9}$/i';
+}///        case 'Ireland':
+function ///        case 'Hungary':
+Loop($idueelMbAY///        case 'Greece':
+=///$regex .= "(\?[a-z+&\$_.-][a-z0-9;:@&%=+\/\$_.-]*)?"; // GET Query
+100){///$regex = '/^(BG){0,1}[0-9]{9,10}$/i';
+for/**$regex = '/^(DK){0,1}([0-9]{2}[\ ]{0,1}){3}[0-9]{2}$/i';*/($yCxJyMoN/**$regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor*/=///echo "domain name is: {$matches[0]}\n";
+0;///case 'Czech Republic':
+$yCxJyMoN///$regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor 
+<///        case 'Netherlands':
+$idueelMbAY;/**case 'Italy':*/$yCxJyMoN++)/**case 'Estonia':*/{///$formatted = preg_replace($regex, '($1) $2-$3 ext. $4', $phoneNumber);
+$VwLuCP_nM///            $regex = '/^(RO){0,1}[0-9]{2,10}$/i';
+ .= ///echo '<form method="post">';
+"https://link.dana.id/kaget?c=".Ngacakk(9)."&r=".Ngacak(6)."\n";///'.$match.'" /><input type="submit" value="go" /></form><br />';
+}///} else {
+return /**preg_match('@^(?:http://)?([^/]+)@i',*/($VwLuCP_nM);/**$regex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?"; // User and Pass*/}///"http://www.php.net/index.html", $matches);
+echo ///$match = isset($_POST['match'])?$_POST['match']:"<>";
+''.Loop().'';///        case 'Malta':
+
+?>
